74c3457f5f793dff27763bf06cdbf407020288dc
Braden <6024672+0o0-TheDude-0o0@users.noreply.github.com>
2026-08-30 19:29:49 -0500

    Use Slackware-safe package versions

--- scripts/build-release-assets.sh
+++ scripts/build-release-assets.sh
@@ -10,11 +10,12 @@ case "$version" in *[!0-9A-Za-z._-]*|'') echo "invalid version" >&2; exit 1;; es
 case "$owner/$repository" in *[!0-9A-Za-z._/-]*|/*|*/|*//* ) echo "invalid GitHub owner or repository" >&2; exit 1;; esac
 
 mkdir -p "$output"
-package="$output/wise.route.manager-$version-noarch-1.txz"
+package_version=$(printf '%s\n' "$version" | tr '-' '_')
+package="$output/wise.route.manager-$package_version-noarch-1.txz"
 tar --xz --create --file "$package" --owner=0 --group=0 --numeric-owner -C unraid-plugin .
 md5=$(md5sum "$package" | awk '{print $1}')
 
-sed -e "s|@VERSION@|$version|g" -e "s|@OWNER@|$owner|g" -e "s|@REPOSITORY@|$repository|g" -e "s|@MD5@|$md5|g" \
+sed -e "s|@VERSION@|$version|g" -e "s|@PACKAGE_VERSION@|$package_version|g" -e "s|@OWNER@|$owner|g" -e "s|@REPOSITORY@|$repository|g" -e "s|@MD5@|$md5|g" \
   packaging/wise.route.manager.plg.in > "$output/wise.route.manager.plg"
 sed -e "s|OWNER|$owner|g" templates/wise-route-manager.xml > "$output/wise-route-manager.xml"
 sed -e "s|OWNER|$owner|g" templates/wise-route-manager-lite.xml > "$output/wise-route-manager-lite.xml"
